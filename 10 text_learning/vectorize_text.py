3d0d41e05d135c4048a0ba64f0f529097759e365
dragonoken <38848443+dragonoken@users.noreply.github.com>
2018-08-05 17:35:57 -0700

    Text Learning
    + some editing to make thing python 3 compatible

---
+++ 10 text_learning/vectorize_text.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+# For compatibilty between python 2 and 3
+from __future__ import print_function
+
+import os
+import pickle
+import re
+import sys
+
+sys.path.append( "../tools/" )
+from parse_out_email_text import parseOutText
+
+"""
+    Starter code to process the emails from Sara and Chris to extract
+    the features and get the documents ready for classification.
+
+    The list of all the emails from Sara are in the from_sara list
+    likewise for emails from Chris (from_chris)
+
+    The actual documents are in the Enron email dataset, which
+    you downloaded/unpacked in Part 0 of the first mini-project. If you have
+    not obtained the Enron email corpus, run startup.py in the tools folder.
+
+    The data is stored in lists and packed away in pickle files at the end.
+"""
+
+load_existing_pickle = True
+
+if load_existing_pickle:
+    word_data = pickle.load(open("your_word_data.pkl", "rb"))
+    from_data = pickle.load(open("your_email_authors.pkl", "rb"))
+
+else:
+    from_sara  = open("from_sara.txt", "r")
+    from_chris = open("from_chris.txt", "r")
+
+    from_data = []
+    word_data = []
+
+    ### temp_counter is a way to speed up the development--there are
+    ### thousands of emails from Sara and Chris, so running over all of them
+    ### can take a long time
+    ### temp_counter helps you only look at the first 200 emails in the list so you
+    ### can iterate your modifications quicker
+    temp_counter = 0
+
+
+    for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
+        for path in from_person:
+            ### only look at first 200 emails when developing
+            ### once everything is working, remove this line to run over full dataset
+            # temp_counter += 1
+            if temp_counter < 200:
+                path = os.path.join('..', path[:-1])
+                ### In case the printing costs too much time or causes crashing problems, comment the this line out
+                print(path)
+                email = open(path, "r")
+
+                ### use parseOutText to extract the text from the opened email
+                extracted_text = parseOutText(email)
+
+                ### use str.replace() to remove any instances of the words
+                ### ["sara", "shackleton", "chris", "germani"]
+                signature_words = ["sara", "shackleton", "chris", "germani"]
+                for signature in signature_words:
+                    extracted_text = extracted_text.replace(signature, '').replace('  ', ' ')
+
+                ### append the text to word_data
+                word_data.append(extracted_text)
+
+                ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+                from_data.append(0 if name == "sara" else 1)
+
+                email.close()
+
+    print("emails processed")
+    from_sara.close()
+    from_chris.close()
+
+    pickle.dump(word_data, open("your_word_data.pkl", "wb"))
+    pickle.dump(from_data, open("your_email_authors.pkl", "wb"))
+
+
+print("\nword_data[152] =", word_data[152])
+print()
+
+
+### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf.fit(word_data)
+print("\n --- Tfidf Vectorized ---")
+print()
+print("Number of different words(features) after Tfidf vectorization:", len(tfidf.get_feature_names()))
+print()
+print("Word number 34597 :", tfidf.get_feature_names()[34597])
--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+# For compatibility between python 2 and 3
+from __future__ import print_function
+
 from nltk.stem.snowball import SnowballStemmer
 import string
 
@@ -25,17 +28,17 @@ def parseOutText(f):
     words = ""
     if len(content) > 1:
         ### remove punctuation
-        text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
+        text_string = content[1].translate(str.maketrans("", "", string.punctuation))
 
         ### project part 2: comment out the line below
-        words = text_string
+        # words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
-
-
+        from nltk.stem.snowball import SnowballStemmer
+        stemmer = SnowballStemmer('english')
+        words = " ".join([stemmer.stem(word) for word in text_string.split()])
 
 
     return words
@@ -43,9 +46,9 @@ def parseOutText(f):
     
 
 def main():
-    ff = open("../text_learning/test_email.txt", "r")
+    ff = open("../10 text_learning/test_email.txt", "r")
     text = parseOutText(ff)
-    print text
+    print(text)
 
 
 
